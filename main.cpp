2ae47434123158aeaebe27a54879918fd6825ac6
Luis Ferreira [1130647] <1130647@isep.ipp.pt>
2014-10-09 10:53:33 +0100

    Inicialização do Repositório.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Miguel
+ *
+ * Created on 9 de Outubro de 2014, 10:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
